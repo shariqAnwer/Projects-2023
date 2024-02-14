b4db5a214200c5be0914df98bab6d452bec6b35c
Md Sharique Anwer <shariqueanwer002@gmail.com>
2024-02-14 16:48:35 +0530

    pages & routing

--- real-estate-marketplace-mern/client/src/App.jsx
+++ real-estate-marketplace-mern/client/src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
     <>
-      <h1 className="text-4xl text-red-500 font-bold underline">
-        Hello world!
-      </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ real-estate-marketplace-mern/client/src/pages/SignUp.jsx
@@ -0,0 +1,5 @@
+const SignUp = () => {
+  return <div>Sign out Page</div>;
+};
+
+export default SignUp;
