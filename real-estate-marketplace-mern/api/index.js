41cd575dd160f66670b603648efbdb8e19ac8321
Md Sharique Anwer <shariqueanwer002@gmail.com>
2024-02-15 20:59:10 +0530

    middleware

--- real-estate-marketplace-mern/api/index.js
+++ real-estate-marketplace-mern/api/index.js
@@ -21,3 +21,14 @@ app.listen(3000, () => console.log("listening on port  3000!!!"));
 
 app.use("/api", userRouter);
 app.use("/api/auth", authRouter);
+
+//middleware to handle possible errors
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
