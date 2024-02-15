cc39c23297c2061d3474d1c62587da122cb9018c
Md Sharique Anwer <shariqueanwer002@gmail.com>
2024-02-15 21:22:10 +0530

    signup page ui

--- real-estate-marketplace-mern/client/src/pages/SignUp.jsx
+++ real-estate-marketplace-mern/client/src/pages/SignUp.jsx
@@ -1,5 +1,44 @@
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
-  return <div>Sign out Page</div>;
+  return (
+    <>
+      <div className="p-3 max-w-lg mx-auto">
+        <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+        <form className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="username"
+            className="border p-3 rounded-lg"
+            id="username"
+          />
+          <input
+            type="email"
+            placeholder="email"
+            className="border p-3 rounded-lg"
+            id="email"
+          />
+          <input
+            type="password"
+            placeholder="password"
+            className="border p-3 rounded-lg"
+            id="password"
+          />
+          <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+            Sign Up
+          </button>
+          <div className="flex mt-3">
+            <p>Have an account?</p>
+            <Link to="/sign-in">
+              <span className=" text-blue-700 hover:underline ml-2">
+                Sign In
+              </span>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default SignUp;
