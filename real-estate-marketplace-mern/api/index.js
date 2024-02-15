d6ade365878352bfb1c05083e09cd5d044601ebe
Md Sharique Anwer <shariqueanwer002@gmail.com>
2024-02-15 16:06:02 +0530

    db connected

--- real-estate-marketplace-mern/api/index.js
+++ real-estate-marketplace-mern/api/index.js
@@ -1,4 +1,17 @@
+import dotenv from "dotenv";
+import { error } from "console";
 import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+// pass&user-shariqueanwer
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((error) => console.log(error));
 
 const app = express();
 
