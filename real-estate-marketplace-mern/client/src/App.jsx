6fb0e4944b3c4033b8e890f26bdec3432425e392
Md Sharique Anwer <shariqueanwer002@gmail.com>
2024-02-15 13:23:38 +0530

    header

--- real-estate-marketplace-mern/client/src/App.jsx
+++ real-estate-marketplace-mern/client/src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sign-in" element={<SignIn />} />
