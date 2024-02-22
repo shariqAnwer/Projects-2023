4ce930f470e1700ca1fef71740762171f36a8307
Md Sharique Anwer <shariqueanwer002@gmail.com>
2024-02-22 14:34:18 +0530

    redux-persist

--- real-estate-marketplace-mern/client/src/redux/store.js
+++ real-estate-marketplace-mern/client/src/redux/store.js
@@ -1,8 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
 import userSlice from "./user/userSlice";
 
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const rootReducer = combineReducers({ user: userSlice });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    user: userSlice,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
