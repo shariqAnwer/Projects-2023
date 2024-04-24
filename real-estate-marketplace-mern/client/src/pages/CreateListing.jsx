6a08582ba08d2ca772dd8666f95569ea696ae82b
Md Sharique Anwer <shariqueanwer002@gmail.com>
2024-04-24 19:27:59 +0530

    upoad image functionality

--- real-estate-marketplace-mern/client/src/pages/CreateListing.jsx
+++ real-estate-marketplace-mern/client/src/pages/CreateListing.jsx
@@ -1,4 +1,84 @@
+/* eslint-disable no-unused-vars */
+
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+
+import { app } from "../firebase";
+import { useState } from "react";
+
 const CreateListing = () => {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+
+  console.log(files);
+  console.log(formData.imageUrls);
+
+  const handleImageSubmit = (e) => {
+    e.preventDefault();
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        // eslint-disable-next-line no-unused-vars
+        .catch((error) =>
+          setImageUploadError("Image upload failed as 3 MB max per image")
+        );
+    } else {
+      setImageUploadError("You can only upload 6 images");
+      setUploading(false);
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Uploading ${Math.floor(progress)}`);
+        },
+        (error) => reject(error),
+        () =>
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+            resolve(downloadURL)
+          )
+      );
+    });
+  };
+
+  //delete image
+  const handleDeleteImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
+  };
+  console.log("hbubffffffffffff", uploading);
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -78,8 +158,6 @@ const CreateListing = () => {
               <input
                 type="number"
                 id="regularPrice"
-                min="1"
-                max="10"
                 required
                 className="p-3 border border-gray-300 rounded-lg"
               />
@@ -92,8 +170,6 @@ const CreateListing = () => {
               <input
                 type="number"
                 id="discountedPrice"
-                min="1"
-                max="10"
                 required
                 className="p-3 border border-gray-300 rounded-lg"
               />
@@ -119,11 +195,40 @@ const CreateListing = () => {
               multiple
               accept="image/*"
               className="p-3 border border-gray-300 rounded w-full"
+              onChange={(e) => setFiles(e.target.files)}
             />
-            <button className="p-3 text-green-700 rounded uppercase border-solid border-2 border-green-700 hover:shadow-lg disabled:opacity-80">
-              Upload
+            <button
+              disabled={uploading}
+              type="button"
+              onClick={handleImageSubmit}
+              className="p-3 text-green-700 rounded uppercase border-solid border-2 border-green-700 hover:shadow-lg disabled:opacity-80"
+            >
+              {uploading ? "Uploading..." : "Upload"}
             </button>
           </div>
+          <p className="text-red-700 text-sm">
+            {imageUploadError && imageUploadError}
+          </p>
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, index) => (
+              <div
+                className="flex justify-between items-center p-3 border"
+                key={url}
+              >
+                <img
+                  src={url}
+                  alt="listing-image"
+                  className="w-21 h-20 object-contain rounded-lg"
+                />
+                <button
+                  type="button"
+                  onClick={() => handleDeleteImage(index)}
+                  className="uppercase text-red-700 hover:opacity-75 p-3 border border-solid rounded-lg"
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
           <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80 ">
             Create Listing
           </button>
